d018b095cbe7e6bd043f665c1564bc86d11c04d3
Ruslan Popov <ruslan.popov@gmail.com>
2012-03-13 07:04:52 +0400

    Re #11: Process activity states.

--- src/ru/securelayer/rad/ando/AndoMain.java
+++ src/ru/securelayer/rad/ando/AndoMain.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.content.Intent;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -36,20 +37,24 @@ import ru.securelayer.rad.ando.MessageAdapter;
 
 public class AndoMain extends Activity
 {
+    private static final String PREFERENCE_FILE = "AndoGettextResourceEditor";
+    private static final String RESOURCE_FILENAME_KEY = "RESOURCE_FILENAME";
+    private static final String RESOURCE_POSITION_KEY = "RESOURCE_POSITION";
+
     private static final int REQUEST_PICK_FILE = 1;
 
-    private String resourceFileName;
-    private TextView textFileName;
-    private Catalog catalog;
-    private ArrayList<Message> messages;
-    private Context ctx;
-    private MessageAdapter pagerAdapter;
-    private ViewPager messagePager;
+    private String resourceFileName = null;
+    private TextView textFileName = null;
+    private Catalog catalog = null;
+    private ArrayList<Message> messages = null;
+    private Context ctx = null;
+    private MessageAdapter pagerAdapter = null;
+    private ViewPager messagePager = null;
 
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle inState) {
+        super.onCreate(inState);
 
         setContentView(R.layout.main);
 
@@ -62,6 +67,40 @@ public class AndoMain extends Activity
         messagePager.setAdapter(pagerAdapter);
 
         textFileName = (TextView) findViewById(R.id.textFileName);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle inState) {
+        super.onRestoreInstanceState(inState);
+    }
+
+    /**
+     * Loads last state of UI and variables.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (readInstanceState(this)) {
+            showNotification(getString(R.string.activity_resumed));
+        }
+    }
+
+    /**
+     * Saves current state of UI and variables.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (this.resourceFileName == null) {
+            if (! writeInstanceState(this)) {
+                showNotification(getString(R.string.activity_saved_not));
+            }
+        }
     }
 
     @Override
@@ -131,6 +170,29 @@ public class AndoMain extends Activity
         startActivityForResult(intent, REQUEST_PICK_FILE);
     }
 
+    public boolean readInstanceState(Context c) {
+        SharedPreferences pref = c.getSharedPreferences(AndoMain.PREFERENCE_FILE, MODE_WORLD_READABLE);
+        if (pref.contains(RESOURCE_FILENAME_KEY) && pref.contains(RESOURCE_POSITION_KEY)) {
+            String filename = pref.getString(RESOURCE_FILENAME_KEY, "");
+            if (! filename.equals("")) {
+                this.loadCatalog(filename);
+                int position = pref.getInt(RESOURCE_POSITION_KEY, 0);
+                messagePager.setCurrentItem(position, true);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public boolean writeInstanceState(Context c) {
+        SharedPreferences pref = c.getSharedPreferences(AndoMain.PREFERENCE_FILE, MODE_WORLD_READABLE);
+        SharedPreferences.Editor editor = pref.edit();
+        int position = this.messagePager.getCurrentItem();
+        editor.putString(RESOURCE_FILENAME_KEY, resourceFileName);
+        editor.putInt(RESOURCE_POSITION_KEY, position);
+        return (editor.commit());
+    }
+
     /**
      * Loads the messages catalog.
      *
@@ -160,8 +222,7 @@ public class AndoMain extends Activity
                 // Show first page
                 messagePager.setCurrentItem(0, true);
                 // Show notification
-                CharSequence msg = getString(R.string.resource_loaded);
-                showNotification(msg);
+                showNotification(getString(R.string.resource_loaded));
             } catch(FileNotFoundException ex) {}
         } catch(IOException ex) {}
     }
