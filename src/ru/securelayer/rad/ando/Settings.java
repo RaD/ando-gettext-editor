f4221d9b8e75e0d7719add598fa0ce389f5b2094
maksim <maksim@HP-ProBook-4530s.(none)>
2012-11-17 13:06:46 +0400

    add google translator and fix bugs

---
+++ src/ru/securelayer/rad/ando/Settings.java
@@ -0,0 +1,19 @@
+package ru.securelayer.rad.ando;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Settings extends PreferenceActivity {
+
+	public static final int REQUESTCODE = 2;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    addPreferencesFromResource(R.xml.settings_pref);
+	    setResult(RESULT_OK);
+	}
+
+}
